6d825539cf8c0b2f746dd2c8b6d6207d7362432d
Guadalupe Soria <A01710797@tec.mx>
2024-08-12 21:01:01 -0600

    declaración de variables, constantes, alcance de las variables declaradas con let, alert, confirm y prompt

--- Chilaquiles/js/introjs.js
+++ Chilaquiles/js/introjs.js
@@ -1,8 +1,8 @@
 // consola (log, info, warn, error, assert)
 
 console.log("hola mundo!");
-console.info("esto es informaciÃ³n importante");
-console.warn("Cuidado!!!");
+console.info("esto es información importante");
+console.warn("Cuidado!!");
 console.error("Ya te equivocaste...");
 console.assert(1 == 1);
 console.assert(1 == 0);
@@ -11,11 +11,11 @@ console.assert(1 == 0);
 // variables, constantes
 
 //let sirve para declarar variables
-let nombre = 'Lalo';
+let nombre = 'Lupita';
 console.log("Hola " + nombre);
 
 //%{nombre_variable} sirve para imprimir el valor de una variable con ``
-console.log(`${nombre} estÃ¡ programando y le estÃ¡ dando hambre`);
+console.log(`${nombre} está programando y le está dando hambre`);
 
 //const sirve para declarar constantes
 const precio_chilaquiles = 89;
@@ -28,16 +28,16 @@ const precio_chilaquiles = 89;
 }
 
 //Arroja un error porque precio_crema fue declarada 
-//en otro Ã¡mbito y se muriÃ³ 2 lÃ­neas arriba
+//en otro ámbito y se murió 2 lí­neas arriba
 //console.log(precio_crema);
 
 
 // alert, prompt, confirm
 alert(`Los chilaquiles cuestan ${precio_chilaquiles}`);
-const chilaquiles_favoritos = prompt("Â¿CuÃ¡les son tus chilaquiles favoritos?");
+const chilaquiles_favoritos = prompt("¿Cuáles son tus chilaquiles favoritos?");
 console.log(`Tus chilaquiles favoritos son ${chilaquiles_favoritos}`);
 
-const is_hambre = confirm("Â¿Tienes hambre?");
+const is_hambre = confirm("¿Tienes hambre?");
 
 if(is_hambre) {
     console.info("Pide unos chilaquiles");
@@ -74,7 +74,7 @@ const arreglo2 = new Array();
 
 arreglo.push("Otro elemento");
 
-arreglo[10] = "Uno mÃ¡s";
+arreglo[10] = "Uno más";
 
 
 //arreglos asociativos
