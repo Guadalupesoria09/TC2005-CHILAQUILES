076dc25bd46df334ef3c9fa55ebbeb7848a1ab52
Guadalupe Soria <A01710797@tec.mx>
2024-08-13 12:35:32 -0600

    generar imagen al azar

--- Chilaquiles/js/poe.js
+++ Chilaquiles/js/poe.js
@@ -1,6 +1,22 @@
 const boton_preparar = document.getElementById('boton_chilaquiles');
 const boton_verde = document.getElementById('boton_salsa_verde');
 const boton_rojo = document.getElementById('boton_salsa_roja');
+const boton_suerte = document.getElementById('boton_suerte');
+
+const chilaquiles = [
+    {
+        descripcion: "Chilaquiles en salsa verde", 
+        imagen: "https://cdn7.kiwilimon.com/recetaimagen/1626/3018.jpg",
+    },
+    {
+        descripcion: "Chilaquiles en salsa roja", 
+        imagen: "https://tse1.mm.bing.net/th?id=OIP.zJcG6SrXwgYRermppNlwJAHaFA&pid=Api&P=0&h=180",
+    },
+    {
+        descripcion: "Chilaquiles con mole", 
+        imagen: "https://cdn7.kiwilimon.com/recetaimagen/14501/960x640/6890.jpg.jpg",
+    },
+]
 
 boton_preparar.onclick = () => {
     const imagen = document.getElementById('imagen_chilaquiles');
@@ -9,20 +25,31 @@ boton_preparar.onclick = () => {
     boton_preparar.style = "display:none";
     boton_verde.style = "display:inline";
     boton_rojo.style = "display:inline";
+    boton_suerte.style = "display:inline";
 }
 
 boton_verde.onclick = () => {
     const imagen = document.getElementById('imagen_chilaquiles');
-    imagen.alt = "Chilaquiles en salsa verde";
-    imagen.src = "https://cdn7.kiwilimon.com/recetaimagen/1626/960x640/3018.jpg.jpg";
+    imagen.alt = chilaquiles[0].descripcion;
+    imagen.src = chilaquiles[0].imagen;
     boton_verde.style = "display:none";
     boton_rojo.style = "display:none";
+    boton_suerte.style = "display:none";
 }
 
 boton_rojo.onclick = () => {
     const imagen = document.getElementById('imagen_chilaquiles');
-    imagen.alt = "Chilaquiles en salsa roja";
-    imagen.src = "https://tse1.mm.bing.net/th?id=OIP.zJcG6SrXwgYRermppNlwJAHaFA&pid=Api&P=0&h=180";
+    imagen.alt = chilaquiles[1].descripcion;
+    imagen.src = chilaquiles[1].imagen;
+    boton_verde.style = "display:none";
+    boton_rojo.style = "display:none";
+    boton_suerte.style = "display:none";
+}
+boton_suerte.onclick = () => {
+    const imagen = document.getElementById('imagen_chilaquiles');
+    const num_chilaquiles = Math.floor(Math.random() * 3);
+    imagen.alt = chilaquiles[num_chilaquiles].descripcion;
+    imagen.src = chilaquiles[num_chilaquiles].imagen;
     boton_verde.style = "display:none";
     boton_rojo.style = "display:none";
 }
