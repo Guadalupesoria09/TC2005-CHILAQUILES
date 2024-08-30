53803e074be552c8f8e1f75b1b417ea0f19833a5
Guadalupe Soria <A01710797@tec.mx>
2024-08-30 01:23:44 -0600

    ejecutar código asincrono

--- Chilaquiles/app.js
+++ Chilaquiles/app.js
@@ -1,9 +1,25 @@
 const filesystem = require('fs'); //fs es el módulo de filesystem
 //Escribe el segundo parámetro en el archivo del primer parámetro
 filesystem.writeFileSync('hola.txt', 'Hola desde node');
-// const arreglo = [5000, 60, 90, 100, 10, 20, 10000, 0, 120, 2000, 340, 1000, 50];
-// for (let item of arreglo) {
-//     setTimeout(() => {
-//         console.log(item);
-//     }, item);
-// } 
+
+//Código asíncrono
+const arreglo = [5000, 60, 90, 100, 10, 20, 10000, 0, 120, 2000, 340, 1000, 50];
+for (let item of arreglo) {
+    setTimeout(() => {
+        console.log(item);
+    }, item);
+} 
+
+setTimeout(() => {
+    console.log("jojo te hackié");
+}, 12000);
+
+
+// const http = require('http');
+// const server = http.createServer( (request, response) => {    
+//     console.log(request.url);
+//     response.setHeader('Content-Type', 'text/html');
+//     response.write("");
+//     response.end();
+// });
+// server.listen(3000);
