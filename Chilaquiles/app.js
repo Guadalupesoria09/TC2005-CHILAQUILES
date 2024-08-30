d9437d4a7f041490229f73785e85b764471c4c43
Guadalupe Soria <A01710797@tec.mx>
2024-08-30 01:54:07 -0600

    Crear servidor web

--- Chilaquiles/app.js
+++ Chilaquiles/app.js
@@ -1,9 +1,12 @@
 const filesystem = require('fs'); //fs es el módulo de filesystem
+
 //Escribe el segundo parámetro en el archivo del primer parámetro
 filesystem.writeFileSync('hola.txt', 'Hola desde node');
 
+
 //Código asíncrono
 const arreglo = [5000, 60, 90, 100, 10, 20, 10000, 0, 120, 2000, 340, 1000, 50];
+
 for (let item of arreglo) {
     setTimeout(() => {
         console.log(item);
@@ -15,11 +18,14 @@ setTimeout(() => {
 }, 12000);
 
 
-// const http = require('http');
-// const server = http.createServer( (request, response) => {    
-//     console.log(request.url);
-//     response.setHeader('Content-Type', 'text/html');
-//     response.write("");
-//     response.end();
-// });
-// server.listen(3000);
+//http es el módulo que contiene todas las funciones de un servidor http
+const http = require('http');
+const server = http.createServer( (request, response) => {    
+     console.log(request.url);
+     response.setHeader('Content-Type', 'text/html');
+     response.write("");
+     response.end();
+ });
+
+
+ server.listen(3000);
