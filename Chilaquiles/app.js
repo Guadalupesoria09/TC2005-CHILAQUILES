7da46e316e8b7f82b537eb269adca3f468fee44e
Guadalupe Soria <A01710797@tec.mx>
2024-09-06 07:31:10 -0600

    configurar carpeta estática para devolver recursos como html, favicon, css, js entre otros

--- Chilaquiles/app.js
+++ Chilaquiles/app.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const app = express();
 
+const path = require('path');
+app.use(express.static(path.join(__dirname, 'public')));
+
 const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({extended: false}));
-
-const path = require('path');
 
 const html_header = `
     
