9529f34db744b1c6efbacf2f05508b6b6834d810
Guadalupe Soria <A01710797@tec.mx>
2024-09-06 02:54:59 -0600

    Página de respuesta para la ruta /preparar

--- Chilaquiles/app.js
+++ Chilaquiles/app.js
@@ -27,7 +27,87 @@ const server = http.createServer( (request, response) => {
     response.setHeader('Content-Type', 'text/html');
     if (request.url == "/preparar") {
 
+        response.write(`
+
+        <!DOCTYPE html>
+        <html>
+            <head>
+                <meta charset="utf-8">
+                <meta name="viewport" content="width=device-width, initial-scale=1">
+                <title>Chilaquiles</title>
+                <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@1.0.2/css/bulma.min.css">
+            </head>
+            <body>
+                <nav class="navbar" role="navigation" aria-label="main navigation">
+                    <div class="navbar-brand">
+                        <img src="https://tse2.mm.bing.net/th?id=OIP.fPCPUSDS8XuKkKOnQBU2BgHaFj&pid=Api&P=0&h=70"> 
+          
+                    <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+                        <span aria-hidden="true"></span>
+                        <span aria-hidden="true"></span>
+                        <span aria-hidden="true"></span>
+                        <span aria-hidden="true"></span>
+                    </a> 
+                </div>
+
+                <div id="navbarBasicExample" class="navbar-menu">
+                    <div class="navbar-start"> 
+                        <a class="navbar-item">
+                        Inicio
+                        </a>
+                  </div>
+                  <div class="navbar-end">
+                    <div class="navbar-item">
+                        <div class="buttons">
+                            <a class="button is-primary">
+                            <strong>Registrarse</strong>
+                            </a>
+                            <a class="button is-light">
+                            Iniciar sesión
+                            </a>
+                        </div>
+                    </div>
+                </div>
+              </div>
+            </nav>
+            <section class="section">
+                    <div class="container">
+                        <header>
+                            <h1 class="title">Preparar chilaquiles :) </h1>
+                        </header>
+                        <main>
+                            <form action="/preparar" method="POST">
+                            <div class="field">
+                                <label class="label" for="nombre">Nombre</label>
+                                <div class="control">
+                                    <input id="nombre" name="nombre" class="input" type="text" placeholder="Lupita Soria">
+                                </div>
+                            </div>
+                                
+                            </form>
+                        </main>
+                        <footer class="footer">
+                            <div class="content has-text-centered">
+                                <p>
+                                    <strong>Bulma</strong> by <a href="https://jgthms.com">Jeremy Thomas</a>.
+                                    The source code is licensed
+                                    <a href="https://opensource.org/license/mit">MIT</a>. The
+                                    website content is licensed
+                                    <a href="https://creativecommons.org/licenses/by-nc-sa/4.0//"
+                                    >CC BY NC SA 4.0</a
+                                    >.
+                                </p>
+                            </div>
+                        </footer>
+                    </div>
+                </section>
+                <script src="js/poe.js"></script>
+            </body>
+        </html>
+        `);
+
     } else {
+
         response.write(`
 
         <!DOCTYPE html>
