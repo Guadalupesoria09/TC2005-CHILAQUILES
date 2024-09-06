3ad18632b67370d88869900d613e539681c2119b
Guadalupe Soria <A01710797@tec.mx>
2024-09-06 02:13:48 -0600

    Laboratorio 10: rutas y formas; responder con html

--- Chilaquiles/app.js
+++ Chilaquiles/app.js
@@ -13,6 +13,7 @@ for (let item of arreglo) {
     }, item);
 } 
 
+
 setTimeout(() => {
     console.log("jojo te hackié");
 }, 12000);
@@ -20,12 +21,113 @@ setTimeout(() => {
 
 //http es el módulo que contiene todas las funciones de un servidor http
 const http = require('http');
+
 const server = http.createServer( (request, response) => {    
-     console.log(request.url);
-     response.setHeader('Content-Type', 'text/html');
-     response.write("");
-     response.end();
- });
+    console.log(request.url);
+    response.setHeader('Content-Type', 'text/html');
+    response.write(`
+    <!DOCTYPE html>
+    <html>
+        <head>
+            <meta charset="utf-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1">
+            <title>Chilaquiles</title>
+            <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@1.0.2/css/bulma.min.css">
+        </head>
+        <body>
+            <nav class="navbar" role="navigation" aria-label="main navigation">
+                <div class="navbar-brand">
+                        <img src="https://tse2.mm.bing.net/th?id=OIP.fPCPUSDS8XuKkKOnQBU2BgHaFj&pid=Api&P=0&h=70"> 
+          
+                    <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+                        <span aria-hidden="true"></span>
+                        <span aria-hidden="true"></span>
+                        <span aria-hidden="true"></span>
+                        <span aria-hidden="true"></span>
+                    </a> 
+                </div>
+
+                <div id="navbarBasicExample" class="navbar-menu">
+                    <div class="navbar-start"> 
+                        <a class="navbar-item">
+                        Inicio
+                        </a>
+                  </div>
+                  <div class="navbar-end">
+                    <div class="navbar-item">
+                        <div class="buttons">
+                            <a class="button is-primary">
+                            <strong>Registrarse</strong>
+                            </a>
+                            <a class="button is-light">
+                            Iniciar sesión
+                            </a>
+                        </div>
+                    </div>
+                </div>
+              </div>
+          </nav>
+        <section class="section">
+            <div class="container">
+                <header>
+                    <h1 class="title">Chilaquiles</h1>
+                </header>
+                <main>
+                    <br>
+                    <p class="block">
+                        <strong>¡hola mundo!</strong> 
+                        Los <span>chilaquiles</span> son <em>deliciosos</em>. 
+                    </p>
+                    <table class="table">
+                        <tbody>
+                            <tr>
+                                <td>Con salsa verde</td>
+                                <td>Con salsa de chipotle</td>
+                            </tr>
+                            <tr>
+                                <td>Crema, queso y aguacate</td>
+                                <td>Crema, queso y aguacate</td>
+                            </tr>
+                        </tbody>
+                        <tfoot>
+                            <td>Nuestros mejores chilaquiles</td>
+                        </tfoot>
+                        <thead>
+                            <tr>
+                                <th>Chilaquiles verdes</th>
+                                <th>Chilaquiles rojos</th>
+                            </tr>
+                        </thead>
+                    </table>
+                    <button class="button is-warning" id="boton_chilaquiles">Preparar orden</button>
+                    <button style="display:none" class="button is-success" id="boton_salsa_verde">Poner salsa verde</button>
+                    <button style="display:none" class="button is-warning" id="boton_suerte">Voy a tener suerte</button>
+                    <button style="display:none" class="button is-danger" id="boton_salsa_roja">Poner salsa roja</button>
+                    <br><br>
+                    <img width="40%" id="imagen_chilaquiles" src="" alt="" />
+                </main>
+                <footer class="footer">
+                    <div class="content has-text-centered">
+                        <p>
+                            <strong>Bulma</strong> by <a href="https://jgthms.com">Jeremy Thomas</a>.
+                            The source code is licensed
+                            <a href="https://opensource.org/license/mit">MIT</a>. The
+                            website content is licensed
+                            <a href="https://creativecommons.org/licenses/by-nc-sa/4.0//"
+                            >CC BY NC SA 4.0</a
+                            >.
+                        </p>
+                    </div>
+                </footer>
+            </div>
+        </section>  
+        <script src="js/poe.js"></script>
+    </body>
+    </html>
+    `);
+    
+    response.end();
+});
 
 
- server.listen(3000);
+server.listen(3000);
