be2a18ff0d618afa77d1cea79e8f4f8afc02ec00
Guadalupe Soria <A01710797@tec.mx>
2024-09-06 04:06:27 -0600

    Refactorizar html header y footer

--- Chilaquiles/app.js
+++ Chilaquiles/app.js
@@ -25,9 +25,8 @@ const http = require('http');
 const server = http.createServer( (request, response) => {    
     console.log(request.url);
     response.setHeader('Content-Type', 'text/html');
-    if (request.url == "/preparar") {
 
-        response.write(`
+    const html_header = `
 
         <!DOCTYPE html>
         <html>
@@ -72,6 +71,34 @@ const server = http.createServer( (request, response) => {
             </nav>
             <section class="section">
                     <div class="container">
+`;
+    const html_footer = `
+                        </main>
+                        <footer class="footer">
+                            <div class="content has-text-centered">
+                                <p>
+                                    <strong>Bulma</strong> by <a href="https://jgthms.com">Jeremy Thomas</a>.
+                                    The source code is licensed
+                                    <a href="https://opensource.org/license/mit">MIT</a>. The
+                                    website content is licensed
+                                    <a href="https://creativecommons.org/licenses/by-nc-sa/4.0//"
+                                    >CC BY NC SA 4.0</a
+                                    >.
+                                </p>
+                            </div>
+                        </footer>
+                    </div>
+                </section>
+                <script src="js/poe.js"></script>
+            </body>
+        </html>
+    `;
+
+    if (request.url == "/preparar") {
+
+        response.write(`
+                        ${html_header}
+
                         <header>
                             <h1 class="title">Preparar chilaquiles :) </h1>
                         </header>
@@ -104,74 +131,14 @@ const server = http.createServer( (request, response) => {
                                 </div>
 
                             </form>
-                        </main>
-                        <footer class="footer">
-                            <div class="content has-text-centered">
-                                <p>
-                                    <strong>Bulma</strong> by <a href="https://jgthms.com">Jeremy Thomas</a>.
-                                    The source code is licensed
-                                    <a href="https://opensource.org/license/mit">MIT</a>. The
-                                    website content is licensed
-                                    <a href="https://creativecommons.org/licenses/by-nc-sa/4.0//"
-                                    >CC BY NC SA 4.0</a
-                                    >.
-                                </p>
-                            </div>
-                        </footer>
-                    </div>
-                </section>
-                <script src="js/poe.js"></script>
-            </body>
-        </html>
+                        ${html_footer}                       
         `);
 
     } else {
 
         response.write(`
-
-        <!DOCTYPE html>
-        <html>
-            <head>
-                <meta charset="utf-8">
-                <meta name="viewport" content="width=device-width, initial-scale=1">
-                <title>Chilaquiles</title>
-                <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@1.0.2/css/bulma.min.css">
-            </head>
-            <body>
-                <nav class="navbar" role="navigation" aria-label="main navigation">
-                    <div class="navbar-brand">
-                        <img src="https://tse2.mm.bing.net/th?id=OIP.fPCPUSDS8XuKkKOnQBU2BgHaFj&pid=Api&P=0&h=70"> 
-          
-                    <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
-                        <span aria-hidden="true"></span>
-                        <span aria-hidden="true"></span>
-                        <span aria-hidden="true"></span>
-                        <span aria-hidden="true"></span>
-                    </a> 
-                </div>
-
-                <div id="navbarBasicExample" class="navbar-menu">
-                    <div class="navbar-start"> 
-                        <a class="navbar-item">
-                        Inicio
-                        </a>
-                  </div>
-                  <div class="navbar-end">
-                    <div class="navbar-item">
-                        <div class="buttons">
-                            <a class="button is-primary">
-                            <strong>Registrarse</strong>
-                            </a>
-                            <a class="button is-light">
-                            Iniciar sesión
-                            </a>
-                        </div>
-                    </div>
-                </div>
-              </div>
-          </nav>
-          <section class="section">
-            <div class="container">
+                
+                ${html_header}
                 <header>
                     <h1 class="title">Chilaquiles</h1>
                 </header>
@@ -185,7 +152,7 @@ const server = http.createServer( (request, response) => {
                         <tbody>
                             <tr>
                                 <td>Con salsa verde</td>
-                                <td>Con salsa de chipotle</td>
+                                <td>Con salsa de mole</td>
                             </tr>
                             <tr>
                                 <td>Crema, queso y aguacate</td>
@@ -210,27 +177,10 @@ const server = http.createServer( (request, response) => {
                     <img width="40%" id="imagen_chilaquiles" src="" alt="" />
                     <p>Anímate a probar suerte con los chilaquiles de la casa</p>
                     <br><br>
-                </main>
-                <footer class="footer">
-                    <div class="content has-text-centered">
-                        <p>
-                            <strong>Bulma</strong> by <a href="https://jgthms.com">Jeremy Thomas</a>.
-                            The source code is licensed
-                            <a href="https://opensource.org/license/mit">MIT</a>. The
-                            website content is licensed
-                            <a href="https://creativecommons.org/licenses/by-nc-sa/4.0//"
-                            >CC BY NC SA 4.0</a
-                            >.
-                        </p>
-                    </div>
-                </footer>
-            </div>
-        </section>  
-        <script src="js/poe.js"></script>
-    </body>
-    </html>
-    `);
-
+                    
+                    ${html_footer}
+                
+ `);
 }
     
     response.end();
