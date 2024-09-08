12b6a433a590e5847e939e1ca03e2185a8c6f860
Guadalupe Soria <A01710797@tec.mx>
2024-09-08 17:01:29 -0600

    Crear modulo users

--- Chilaquiles/app.js
+++ Chilaquiles/app.js
@@ -10,6 +10,13 @@ app.use(express.static(path.join(__dirname, 'public')));
 const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({extended: false}));
 
+const session = require('express-session');
+app.use(session({
+    secret: 'string secreto largo aleatorio ...ejemplo', 
+    resave: false, //La sesión no se guardará en cada petición, sino sólo se guardará si algo cambió 
+    saveUninitialized: false, //Asegura que no se guarde una sesión para una petición que no lo necesita
+}));
+
 //Middleware
 app.use((request, response, next) => {
     console.log('Middleware!');
@@ -20,15 +27,16 @@ app.use('/about', (request, response, next) => {
     response.sendFile(path.join(__dirname, 'old_labs', 'about.html'));
 });
 
+const usersRutas = require('./routes/users.routes');
+app.use('/users', usersRutas);
+
 const prepararRutas = require('./routes/preparar.routes');
-
 app.use('/preparar', prepararRutas);
-const chilaquilesRutas = require('./routes/chilaquiles.routes');
 
+const chilaquilesRutas = require('./routes/chilaquiles.routes');
 app.use('/', chilaquilesRutas);
 
 app.use((request, response, next) => {
-
     response.statusCode = 404;
     response.render('404');
 })
---
+++ Chilaquiles/controllers/user.controller.js
@@ -0,0 +1,8 @@
+exports.get_login = (request, response, next) => {
+};
+exports.post_login = (request, response, next) => {
+    
+};
+exports.get_logout = (request, response, next) => {
+    
+};
